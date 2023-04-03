68cc3f028c03f9a83b0567bfe8237ce192f1b555
RoboticInsomnia <RoboticInsomniaco@gmail.com>
2023-04-03 18:23:59 -0400

    adding files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json; // to use Newtonsoft.Json API
+
+namespace Nuget.Quickstart
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Name = "Orion Dev",
+                Email = "Orion@nuget.org",
+                DOB = new DateTime(1984, 11, 19, 0, 0, 0, DateTimeKind.Utc), 
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+### Learning NuGet
+
+www.nuget.org 
+
+NuGet package is a single ZIP file with the .nupkg extension that contains compiled code (DLLs), other files related to that code and a descriptive manifest that includes info like the following: package version and number.
+
+You can find the 100K unique packages at nuget.org.
+
+Packages are provided by millions of .NET/.NET Core developers daily.
+
